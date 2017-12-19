273a42e6e2eafa2f00f0711f14662697a9ed5a89
Jason Kristian <jasonk@apps4u.com.au>
2017-12-19 16:20:49 +1000

    Initial Commit

---
+++ mmmm/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  mmmm
+//
+//  Created by Administrator on 19/12/17.
+//  Copyright © 2017 Apps 4 U Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
